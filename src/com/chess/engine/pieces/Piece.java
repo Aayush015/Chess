3369da3611338e4e33d884f661b08d1fe37441aa
Aayush015 <aayushkafle308@gmail.com>
2024-03-18 18:49:43 -0600

    Added new classes (empty)

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,7 +1,28 @@
 package com.chess.engine.pieces;
+import com.chess.engine.Type;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Board;
+
+import java.util.List;
+
+
 /**
  * Represent a particular Piece within a Tile.
+ * Each piece type will extend this Piece class to have its abstract characteristics.
  */
+public abstract class Piece {
+    protected final int piecePosition;
+    protected final Type pieceType;
 
-public class Piece {
+    Piece(final int piecePosition, final Type pieceType){
+        this.pieceType = pieceType;
+        this.piecePosition = piecePosition;
+    }
+
+    /*
+    Calculate a list of legal moves for a particular piece
+     */
+    public abstract List<Move> calculateLegalMoves (final Board board);
+
+
 }
